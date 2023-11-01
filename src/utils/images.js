c0f83019dd674fcd2c9890edbf83f0be29213c4d
iamshivampandey <itsshivampandey551@gmail.com>
2023-11-01 11:31:40 -0700

    Add all images and icons

--- src/utils/images.js
+++ src/utils/images.js
@@ -9,13 +9,26 @@ export const profile_tab_img = require('./../assets/tabIcons/profile_tab_img.png
 export const onBoardingBgImage = require('./../assets/onBoardingImages/onBoardingBgImage.png');
 export const powerdopLogo = require('./../assets/onBoardingImages/powerdopLogo.png');
 
+// png icon
+export const adminid_image = require('./../assets/images/adminid_image.png');
+export const Ellipse_image = require('./../assets/images/Ellipse_image.png');
+export const google_image = require('./../assets/images/google_image.png');
+export const numberpad_image = require('./../assets/images/numberpad_image.png');
+export const outlook_image = require('./../assets/images/outlook_image.png');
+
 // screen icons
-// export const admin_ic = require('./../assets/icons/admin_ic.svg');
 export const calendar_ic = require('./../assets/icons/calendar_ic.svg');
 export const comment_ic = require('./../assets/icons/comment_ic.svg');
-// export const facebook_ic = require('./../assets/icons/facebook_ic.svg');
-// export const google_ic = require('./../assets/icons/google_ic.svg');
-// export const muscle_ic = require('./../assets/icons/muscle_ic.svg');
-// export const numberpad_ic = require('./../assets/icons/numberpad_ic.svg');
 export const save_ic = require('./../assets/icons/save_ic.svg');
-
+// export const muscle_ic = require('./../assets/icons/muscle_ic.svg');
+export const add_post_ic = require('./../assets/icons/add_post_ic.svg');
+export const add_with_ellipse_ic = require('./../assets/icons/add_with_ellipse_ic.svg');
+export const bell_ic = require('./../assets/icons/bell_ic.svg');
+export const chat_ic = require('./../assets/icons/chat_ic.svg');
+export const gift_ic = require('./../assets/icons/gift_ic.svg');
+export const link_ic = require('./../assets/icons/link_ic.svg');
+export const more_horizontal_ic = require('./../assets/icons/more_horizontal_ic.svg');
+export const right_arrow_ellipse_ic = require('./../assets/icons/right_arrow_ellipse_ic.svg');
+export const setting_ic = require('./../assets/icons/setting_ic.svg');
+// export const share_ic = require('./../assets/icons/share_ic.svg');
+export const youtube_activity_ic = require('./../assets/icons/youtube_activity_ic.svg');
